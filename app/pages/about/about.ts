134e8fed9e8a005e3356bbaad950f389bc9b11d5
LazyJoJo <joec921@gmail.com>
2016-07-01 01:48:54 +0800

    add fixer.io data source

--- app/pages/about/about.ts
+++ app/pages/about/about.ts
@@ -1,10 +1,30 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {CurrencyService} from '../../providers/currency-service/currency-service';
 
 @Component({
-  templateUrl: 'build/pages/about/about.html'
+  templateUrl: 'build/pages/about/about.html',
+  providers:[CurrencyService]
 })
 export class AboutPage {
-  constructor(private navController: NavController) {
+
+  public currency:any;
+
+  constructor(private navController: NavController,public currencyService:CurrencyService) {
+
+      this.loadCurrencyData();
+
   }
+
+  loadCurrencyData(){
+
+      this.currencyService.load()
+      .then(data =>{
+      this.currency = data;
+      console.log(this.currency);
+
+      });
+
+  }
+
 }
